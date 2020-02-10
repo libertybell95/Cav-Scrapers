756f22ad6f32f0ef7eef62712d404d6d636f5ffb
Joshua Bell <joshuakbell@gmail.com>
2020-02-10 15:23:51 -0600

    Add CSV bulk addition functions

--- milpacEditor.py
+++ milpacEditor.py
@@ -9,18 +9,6 @@ import re
 
 import requests
 
-'''
-Structure
-    class add():
-        def serviceRecord(text, date, citation=False)
-        def award(award, date, citation, details=False)
-        def uniform(file, deleteCurrent=True)
-    class edit()
-        def user(rank=False, position=False, secondaryPositions=False, enlistmentDate=False, promotionDate=False) # All others should not be changed via script
-        def serviceRecord(text=False, date=False, citation=False)
-        def award(award=False, date=False, citation=False, details=False)
-'''
-
 class add:
     def __init__(self, credentialsJSON=False):
         '''
@@ -43,7 +31,7 @@ class add:
         }
 
         self.s.post("https://7cav.us/login/login", data=auth, allow_redirects=False)
-        self.hiddenToken = False
+        self.hiddenToken = False 
     
     def serviceRecord(self, milpacID, roster, text, date, citationFile=False):
         '''
@@ -53,8 +41,8 @@ class add:
             milpacID (int): Milpac ID of trooper.
             roster (int): Roster number trooper is in (Check URL. Combat Roster is 1)
             text (str): Service record entry.
-            date (str): Date of service record. Format is yyyy-mm-dd (Example: 7 February 2020 would be 2020-02-07).
-            citationFile (str) [OPTIONAL]: Path to citation file, if needed. (CITATION FILE CURRENTLY NOT SUPPORTED)
+            date (str): Date of service record. Format is yyyy-mm-dd (Example: "2020-02-19").
+            citationFile (str) [OPTIONAL]: Path to citation file, if needed.
 
         Output (bool): Returns True if record created successfully, False if unsuccessful.
         '''
@@ -94,7 +82,7 @@ class add:
         else:
             print(f"Entry not submitted. HTTP Error {post.status_code}")
             return False
-
+    
     def award(self, milpacID, roster, award, date, citationFile=False, details=False):
         '''
         Create award entry.
@@ -103,7 +91,7 @@ class add:
             milpacID (int): Milpac ID of trooper.
             roster (int): Roster number trooper is in (Check URL. Combat Roster is 1)
             award (str): Full name of award, as it appears on a trooper's milpacs.
-            date (str): Date of service record. Format is yyyy-mm-dd (Example: 7 February 2020 would be 2020-02-07).
+            date (str): Date of service record. Format is yyyy-mm-dd (Example: "2020-02-19").
             citationFile (str): Path to citation file.
             details (str) [OPTIONAL]: Details text, if needed.
         '''
@@ -144,7 +132,7 @@ class add:
         else:
             print(f"Award not submitted. HTTP Error {post.status_code}")
             return False
-
+    
     def uniform(self, milpacID, roster, uniformfile, deleteCurrent=True):
         '''
         Upload a new uniform.
@@ -172,20 +160,96 @@ class add:
         else:
             print(f"Uniform not submitted. HTTP Error {post.status_code}")
             return False
-
+   
     def __del__(self):
         self.s.close()
 
+class bulkAdd:
+    def __init__(self):
+        self.s = add()
+    
+    def serviceRecords(self, csvFile):
+        '''
+        Add a large amount of service record entries from a .csv file.
+
+        Inputs:
+            csvFile (str): Path to .csv file.
+
+        Each row on the csv file must have the folling format:
+            0 (int): Trooper's Milpac ID.
+            1 (int): Trooper's roster ID.
+            2 (str): Service record entry text.
+            3 (str): Service record entry date. Must be following Format: yyyy-mm-dd (Ex: "2020-01-19")
+            4 (str) [OPTIONAL]: Path to citation file. Not the folder, the actual file.
+        '''
+
+        with open(csvFile) as file:
+            records = list(csv.reader(file))
+
+        for r in records:
+            assert (len(r) in (4,5)), f"Entry is wrong length, needs to be 4 or 5. Current length: {len(r)}. Row contents:\n{r}"
+            if len(r) == 4:
+                self.s.serviceRecord(r[0], r[1], r[2], r[3])
+            else:
+                self.s.serviceRecord(r[0], r[1], r[2], r[3], r[4])
+
+    def awards(self, csvFile):
+        '''
+        Add a large amount of awards from a .csv file.
+
+        Inputs:
+            csvFile (str): Path to .csv file.
+
+        Each row on the csv file must have the folling format:
+            0 (int): Trooper's Milpac ID.
+            1 (int): Trooper's roster ID.
+            2 (str): Award name, as it appears in Milpacs. Case sensitive.
+            3 (str): Service record entry date. Must be following Format: yyyy-mm-dd (Ex: "2020-01-19").
+            4 (str): Path to citation file. Not the folder, the actual file.
+            5 (str) [OPTIONAL]: Award details.
+        '''
+
+        with open(csvFile) as file:
+            awards = list(csv.reader(file))
+
+        for a in awards:
+            assert (len(a) in (5,6)), f"Entry is wrong length, needs to be 5 or 6. Current length: {len(a)}. Row contents:\n{a}"
+            if len(a) == 5: # If award details not given.
+                self.s.award(a[0], a[1], a[2], a[3], a[4])
+            else: # if award details is given.
+                self.s.award(a[0], a[1], a[2], a[3], a[4], a[5])
+    
+    def uniforms(self, csvFile):
+        '''
+        Add a large amount of uniforms from a .csv file.
+
+        Inputs:
+            csvFile (str): Path to .csv file.
+
+        Each row on the csv file must have the folling format:
+            0 (int): Trooper's Milpac ID.
+            1 (int): Trooper's roster ID.
+            2 (str): Path to uniform file. The file, not the folder.
+        '''
+
+        with open(csvFile) as file:
+            uniforms = list(csv.reader(file))
+
+        for u in uniforms:
+            assert (len(u) == 3), f"Entry is wrong length, needs to be 3. Current length {len(u)}. Row contents:\n{u}" # If the length of the row is too long, throw error.
+            self.s.uniform(u[0], u[1], u[2])
 
 if __name__ == "__main__":
-    print("This functionality only supports adding bulk service records. If this is not your intention press CTRL+C now.")
-    path = input("Enter absolute path to .csv file: ")
+    choice = int(input("What type of bulk addition would you like to execute:\n1 - Service Records\n2 - Awards\n3 - Uniforms\n Enter a number: "))
 
-    with open(path) as file:
-        items = list(csv.reader(file))
-
-    for i in items:
-        # print(i)
-        assert (len(i) == 4) # Check for proper input length.
-        instance = add()
-        instance.serviceRecord(int(i[0]), int(i[1]), str(i[2]), str(i[3]))
+    if choice == 1:
+        print("You chose Service Records.")
+        path = input("Enter full path to .csv file. The file not the folder:\n")
+    elif choice == 2:
+        print("You chose Awards.")
+        path = input("Enter full path to .csv file. The file not the folder:\n")
+    elif choice == 3:
+        print("You chose Uniforms")
+        path = input("Enter full path to .csv file. The file not the folder:\n")
+    else:
+        print("That is not a choice")
